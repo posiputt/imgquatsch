2ca7d35721262a5c4af953473cfb143cfa0b6545
posiputt <i@posiputt.de>
2015-01-08 05:47:44 +0100

    added time/ETA feature, removed scrolling output

--- imgsquash.py
+++ imgsquash.py
@@ -1,12 +1,15 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
 import sys
+import time
 from PIL import Image
 
 # TODO test for images
 
-batchlen = 100 # max by PIL about 1000 but excessive swapping occurs earlier (ca. 200 on a 4gb ram machine)
-resize_width = 10 
+batchlen = 10 # max by PIL about 1000 but excessive swapping occurs earlier (ca. 200 on a 4gb ram machine)
+resize_width = 1
 
-print "reading args..."
+print "\t"+"reading args..."
 image_names = []
 for i, a in enumerate(sys.argv):
     if i!=0:
@@ -17,14 +20,17 @@ height = tmpimg.size[1]
 out = Image.new("RGB", (len(image_names)*resize_width, height))
 
 batchcount = len(image_names)/batchlen
-print "squashing %d images into one" % len(image_names)
-prompt = raw_input("continue? (y/n): ").lower()
+print "\t"+"squashing %d images into one" % len(image_names)
+prompt = raw_input("\tcontinue? (y/n): ").lower()
 
 if prompt != "y":
     sys.exit("aborted by user")
+    
+starttime = time.time()
 
 for index in range(0, len(image_names), batchlen):
-    print "opening files... (batch %d of %d) " % ((index/batchlen), batchcount),
+    # open files
+    sys.stdout.write("\r\t"+"batch %d of %d" % ((index/batchlen)+1, batchcount+1))
     images = []
     if index+batchlen < len(image_names):
         end = index+batchlen
@@ -34,14 +40,20 @@ for index in range(0, len(image_names), batchlen):
     for i in range(index, end):
         images.append(Image.open(image_names[i]))
 
-    print "resizing images... ",
+    # resize images
     lines = []
     for i in images:
         lines.append(i.resize((resize_width, height), Image.ANTIALIAS))
 
-    print "concatenating... "
+    # concatenate
     for i, l in enumerate(lines):
         out.paste(l, (((index+i)*resize_width), 0))
+        
+    now = time.time()
+    runtime = now-starttime
+    etl = batchcount * runtime / ((index/batchlen)+1)
+    sys.stdout.write(" / "+"%02d:%02d / %02d:%02d" % (runtime/60, runtime%60, etl/60, etl%60))
+    sys.stdout.flush()
 
-print "saving out.png..."
+print "\n"+"saving out.png..."
 out.save("out.png")
